6f61d53305e2ebb853047cbb404d8d04e6fc88d8
alexandrereys <anr.alexandre@gmail.com>
2024-04-15 16:05:08 -0300

    apply eslint auto format

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from "express";
+import express from 'express';
 
-import { config } from "dotenv";
-import routes from "./routes";
+import { config } from 'dotenv';
+import routes from './routes';
 
 config();
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from "express";
-import { Router } from "express";
+import { Request, Response } from 'express';
+import { Router } from 'express';
 const routes = Router();
 
-routes.get("/", (req: Request, res: Response) => {
-  res.send("hello world !!");
+routes.get('/', (req: Request, res: Response) => {
+    res.send('hello world !!');
 });
 
 export default routes;
