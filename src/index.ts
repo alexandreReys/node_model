6a5087efde9f9b9c8b2173b9a9200a3ea2eddf64
alexandrereys <anr.alexandre@gmail.com>
2024-04-15 09:00:10 -0300

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+import { config } from "dotenv";
+import routes from "./routes";
+
+config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+import { Router } from "express";
+const routes = Router();
+
+routes.get("/", (req: Request, res: Response) => {
+  res.send("hello world !!");
+});
+
+export default routes;
